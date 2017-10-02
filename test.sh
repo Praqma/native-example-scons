0892e83db6bda79dc884891c4c9a9abf10b7fd34
Leonard Lee <leonard.lee@praqma.com>
2017-10-02 21:09:32 +0200

    Split commands.

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 export WORKSPACE_DIR=/workspace
 
 if [ "$PWD" != "$WORKSPACE_DIR" ]; then
-  echo "Build helper script to be run inside a container."
+  echo "Test helper script to be run inside a container."
   echo "Go to $WORKSPACE_DIR directory before running this script."
   exit 1
 fi
@@ -15,4 +15,4 @@ git submodule update --init --force
 
 scons
 
-${WORKSPACE_DIR}/out/bin/main
+${WORKSPACE_DIR}/out/bin/test
